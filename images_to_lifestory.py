57f67f44eaa675c689a81e0c636430a1a9d32ff3
abhi9384 <abhinav984@gmail.com>
2024-09-30 20:25:55 +0300

    CSS added

--- images_to_lifestory.py
+++ images_to_lifestory.py
@@ -23,8 +23,10 @@ role = "You are a fascinating story teller who can write life stories of a perso
 task = f"""You will be provided different images of same person. Along with the person's images, some description of that image may or may not be provided.
         If no descriptions are given about images provided, you can use your imagination to come up with interesting narration.
         If descriptions are given for images, you should use those descriptions along with your imagination to come up with fascinating life story.
-        I want you to write a fascinating life story for a person whose images are given in 100 words or less."""
+        I want you to write a fascinating life story for a person whose images are given."""
 
+# Sidebar inputs
+st.sidebar.header("Input Details")
 person_name = st.sidebar.text_input("Name of the person")
 with st.sidebar:
        person_gender = st.radio("Gender of the person", ("Male", "Female"))
@@ -33,50 +35,42 @@ instruction0 = f"Name of the person is {person_name} and gender is {person_gende
 
 content = [role, task, instruction0]
 
-uploaded_pics = st.file_uploader(label="Picture 1", type=["png", "jpg", "jpeg"], accept_multiple_files=True)
+# File uploader and description input
+uploaded_pics = st.file_uploader(label="Pictures", type=["png", "jpg", "jpeg"], accept_multiple_files=True)
 pics_description = st.text_area(label="Please provide description of pictures uploaded and anything else you want to mention about the person")
 
 submit_button = st.button(label="Generate Lifestory", key="GenLifeStory")
 
+# List to hold images
 pics_list = []
 
 if submit_button:
-
-        #st.write('No of pics uploaded: ', len(uploaded_pics))
-
+    if uploaded_pics:
         for uploaded_pic in uploaded_pics:
-                pic = PIL.Image.open(uploaded_pic)
-                #st.image(pic) # to display image
-                
-                pics_list.append(pic)
-        
-        #st.write('pics_list: ', pics_list)
-        for item in pics_list:
-                 if item!= None:
-                         content.append(item)
+            pic = PIL.Image.open(uploaded_pic)
+            pics_list.append(pic)
 
-        if pics_description != None:
-               content.append(pics_description)
+        # Add images and descriptions to content
+        content.extend(pics_list)
+        if pics_description:
+            content.append(pics_description)
 
+        # Generate response from AI model
         response = model.generate_content(content)
         llm_text_response = response.text
 
+        # Display results
         st.title(":blue[Lifestory] :sunglasses:")
+        st.markdown("### Generated Lifestory")
+        st.markdown(llm_text_response)
+        
+        # Display uploaded images
+        st.markdown("### Uploaded Pictures")
+        cols = st.columns(len(pics_list))
+        for col, img in zip(cols, pics_list):
+            col.image(img, use_column_width=True)
 
-        with st.container():
-                st.markdown(llm_text_response)
+        st.success("Lifestory generated successfully!")
 
-
-
-
-# Language in which you want to convert
-#language = 'en'
-
-# Passing the text and language to the engine, 
-# here we have marked slow=False. Which tells 
-# the module that the converted audio should 
-# have a high speed
-#audio_file = gTTS(text=llm_text_response, lang=language, slow=False)
-
-# Saving the converted audio in a mp3 file named
-#audio_file.save("Elly.mp3")
+# Footer
+st.markdown('<div style="text-align: center; margin-top: 50px; font-size: 14px; color: #888;">© 2024 Your Company. All rights reserved.</div>', unsafe_allow_html=True)
