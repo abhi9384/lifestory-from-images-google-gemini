c5a3d0cfca16bf91aa59073068bec00a064c8d33
abhi9384 <abhinav984@gmail.com>
2024-09-30 20:10:35 +0300

    Streamlit code added

--- images_to_lifestory.py
+++ images_to_lifestory.py
@@ -1,15 +1,19 @@
 from IPython.display import display
-from dotenv import load_dotenv
+import streamlit as st
 import google.generativeai as genai
-import os
 import PIL.Image
+import numpy as np
 from gtts import gTTS
 import warnings
+import os
+from dotenv import load_dotenv
 
 load_dotenv()
 
 # Settings the warnings to be ignored 
 warnings.filterwarnings('ignore') 
+
+st.set_page_config(layout="wide", page_title="Lifestory from Images")
 
 genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
 model = genai.GenerativeModel("gemini-1.5-flash")   #'gemini-1.0-pro-vision-001'
@@ -19,34 +23,76 @@ role = "You are a fascinating story teller who can write life stories of a perso
 task = f"""You will be provided different images of same person. Along with the person's images, some description of that image may or may not be provided.
         If no descriptions are given about images provided, you can use your imagination to come up with interesting narration.
         If descriptions are given for images, you should use those descriptions along with your imagination to come up with fascinating life story.
-        I want you to write a fascinating life story for a person whose images are given."""
+        I want you to write a fascinating life story for a person whose images are given in 100 words or less."""
 
-instruction1 = "Please write a story about a young girl. Her name is Elly. She was born in Singapore. Her pic when she was born: "
-pic1 = PIL.Image.open("IMG-20151222-WA0003.jpg")
+person_name = st.sidebar.text_input("Name of the person")
+with st.sidebar:
+       person_gender = st.radio("Gender of the person", ("Male", "Female"))
 
-instruction2 = "Her picture at the age of 1.5 years in Malaysia Hotel during vacation: "
-pic2 = PIL.Image.open('IMG_0083.JPG')
+instruction0 = f"Name of the person is {person_name} and gender is {person_gender}. "
 
-instruction3 = "Her picture at the age of 3 years on her first cruise trip to Bali from Singapore: "
-pic3 = PIL.Image.open('IMG_0764.JPG')
+content = [role, task, instruction0]
 
-instruction4 = "Her picture at the age of 4.5 years in her first school's Father's day celebration: "
-pic4 = PIL.Image.open('FRSA4569.JPG')
+uploaded_pics = st.file_uploader(label="Picture 1", type=["png", "jpg", "jpeg"], accept_multiple_files=True)
+pics_description = st.text_area(label="Please provide description of pictures uploaded and anything else you want to mention about the person")
 
-content = [role, task, instruction1, pic1, instruction2, pic2, instruction3, pic3, instruction4, pic4]
+submit_button = st.button(label="Generate Lifestory", key="GenLifeStory")
 
-response = model.generate_content(content)
-llm_text_response = response.text
-print("Lifestory: ", llm_text_response)
+pics_list = []
+
+if submit_button:
+
+        #st.write('No of pics uploaded: ', len(uploaded_pics))
+
+        for uploaded_pic in uploaded_pics:
+                pic = PIL.Image.open(uploaded_pic)
+                #st.image(pic) # to display image
+                
+                pics_list.append(pic)
+        
+        #st.write('pics_list: ', pics_list)
+        for item in pics_list:
+                 if item!= None:
+                         content.append(item)
+
+        if pics_description != None:
+               content.append(pics_description)
+
+        response = model.generate_content(content)
+        llm_text_response = response.text
+
+        st.title(":blue[Lifestory] :sunglasses:")
+
+        with st.container():
+                st.markdown(llm_text_response)
+
+
+
 
 # Language in which you want to convert
-language = 'en'
+#language = 'en'
 
 # Passing the text and language to the engine, 
 # here we have marked slow=False. Which tells 
 # the module that the converted audio should 
 # have a high speed
-audio_file = gTTS(text=llm_text_response, lang=language, slow=False)
+#audio_file = gTTS(text=llm_text_response, lang=language, slow=False)
 
 # Saving the converted audio in a mp3 file named
-audio_file.save("Elly.mp3")
+#audio_file.save("Elly.mp3")
+
+
+############################### Old code
+#instruction1 = "Her pic when she was born: "
+#pic1 = PIL.Image.open("IMG-20151222-WA0003.jpg")
+
+# instruction2 = "Her picture at the age of 1.5 years in Malaysia Hotel during vacation: "
+# pic2 = PIL.Image.open('IMG_0083.JPG')
+#uploaded_pic2 = st.file_uploader(label="Picture 2", type=["png", "jpg", "jpeg"])
+#instruction2 = st.text_input(label="Description of Picture 2")
+
+# instruction3 = "Her picture at the age of 3 years on her first cruise trip to Bali from Singapore: "
+# pic3 = PIL.Image.open('IMG_0764.JPG')
+
+# instruction4 = "Her picture at the age of 4.5 years in her first school's Father's day celebration: "
+# pic4 = PIL.Image.open('FRSA4569.JPG')
